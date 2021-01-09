0471b85493ad7d87722a62d456dd5a62cbde2f8f
CJ Virtucio <cjv287@gmail.com>
2021-01-09 17:15:47 -0500

    mvp

--- pdf-cli.py
+++ pdf-cli.py
@@ -1,8 +1,50 @@
+import sys
+
 import click
+import PyPDF2
 
 @click.command()
-def main():
-    print("hello")
+@click.option(
+    '-s',
+    '--src',
+    required=True,
+    type=str,
+)
+@click.option(
+    '-d',
+    '--dest',
+    default=None,
+    type=str,
+)
+@click.option(
+    '-p',
+    '--start',
+    default=0,
+    type=int,
+)
+@click.option(
+    '-c',
+    '--count',
+    default=-1,
+    type=int,
+)
+def main(src, dest, start, count):
+    with open(src, 'rb') as src_pdf:
+        reader = PyPDF2.PdfFileReader(src_pdf)
+
+        output_stream = sys.stdout
+        try:
+            if dest is not None:
+                output_stream = open(dest, 'wb+')
+            writer = PyPDF2.PdfFileWriter()
+            count = count if count > 0 else reader.numPages
+            i = 0
+            while i < count:
+                writer.addPage(reader.getPage(i))
+                i += 1
+            writer.write(output_stream)
+        finally:
+            output_stream.close()
 
 if __name__ == '__main__':
     main()
