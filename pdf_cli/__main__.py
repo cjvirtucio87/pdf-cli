393d03c0d09ced09489ed2f6a3d6c9f9b096895f
CJ Virtucio <cjv287@gmail.com>
2021-12-31 10:42:40 -0500

    refactored to module and click group

--- pdf_cli/__main__.py
+++ pdf_cli/__main__.py
@@ -1,9 +1,20 @@
+"""
+Entrypoint for app.
+"""
+
 import sys
 
 import click
 import PyPDF2
 
-@click.command()
+@click.group()
+def main():
+    """
+    Simple python app for managing PDF files.
+    """
+    return
+
+@main.command()
 @click.option(
     '-s',
     '--src',
@@ -28,26 +39,26 @@ import PyPDF2
     default=-1,
     type=int,
 )
-def main(src, dest, start, count):
+def crop(src, dest, start, count):
+    """
+    Crop COUNT number of pages from pdf file at SRC into DEST, starting from page START.
+    """
     with open(src, 'rb') as src_pdf:
         reader = PyPDF2.PdfFileReader(src_pdf)
 
-        output_stream = sys.stdout
-        try:
-            if dest is not None:
-                output_stream = open(dest, 'wb+')
+        with (open(dest, 'wb+') \
+                if dest is not None \
+                else sys.stdout) as output_stream:
             writer = PyPDF2.PdfFileWriter()
             count = count if count > 0 else reader.numPages
 
             if start < 0 or (start + count) >= reader.numPages:
-                raise ValueError("invalid start: %d" % start)
+                raise ValueError(f"invalid start: {start}")
             i = start
             while i < (start + count):
                 writer.addPage(reader.getPage(i))
                 i += 1
             writer.write(output_stream)
-        finally:
-            output_stream.close()
 
 if __name__ == '__main__':
     main()
