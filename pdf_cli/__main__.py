9e6bc4c512ec216ca0ea1e3fc70b07598909e445
CJ Virtucio <cjv287@gmail.com>
2021-12-31 10:58:18 -0500

    alphabetized args; added --skip flag for indicating pages to skip

--- pdf_cli/__main__.py
+++ pdf_cli/__main__.py
@@ -7,6 +7,15 @@ import sys
 import click
 import PyPDF2
 
+def _to_skip_set(skip):
+    if skip is None:
+        return skip
+
+    return {
+        int(page_number_string)
+        for page_number_string in skip.split(',')
+    }
+
 @click.group()
 def main():
     """
@@ -16,33 +25,50 @@ def main():
 
 @main.command()
 @click.option(
-    '-s',
-    '--src',
-    required=True,
-    type=str,
+    '-c',
+    '--count',
+    default=-1,
+    help="Number of pages from START to crop (default: page count of SRC).",
+    type=int,
 )
 @click.option(
     '-d',
     '--dest',
     default=None,
+    help="Destination for the output PDF (default: STDOUT).",
+    type=str,
+)
+@click.option(
+    '-s',
+    '--src',
+    help="Source PDF.",
+    required=True,
     type=str,
 )
 @click.option(
     '-p',
     '--start',
+    help="Starting page for the cropped output (default: first page of SRC).",
     default=0,
     type=int,
 )
 @click.option(
-    '-c',
-    '--count',
-    default=-1,
-    type=int,
+    '-k',
+    '--skip',
+    help=(
+        "Comma separated list of page numbers in SRC to skip. "
+        "The page numbers are based on the original SRC PDF, not "
+        "the output DEST. An example would be '0,5,9,13'"
+    ),
+    default=None,
+    type=str,
 )
-def crop(src, dest, start, count):
+def crop(count, dest, src, start, skip):
     """
     Crop COUNT number of pages from pdf file at SRC into DEST, starting from page START.
     """
+    skip_set = _to_skip_set(skip)
+
     with open(src, 'rb') as src_pdf:
         reader = PyPDF2.PdfFileReader(src_pdf)
 
@@ -56,7 +82,9 @@ def crop(src, dest, start, count):
                 raise ValueError(f"invalid start: {start}")
             i = start
             while i < (start + count):
-                writer.addPage(reader.getPage(i))
+                if skip_set is None or i not in skip_set:
+                    writer.addPage(reader.getPage(i))
+
                 i += 1
             writer.write(output_stream)
 
